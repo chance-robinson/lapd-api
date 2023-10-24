38bdde97b5e3b7f613f497e075fc4acdf2c5afde
Chance Robinson <brighamyoung2@gmail.com>
2023-10-23 21:19:57 -0600

    added ml prediction

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 import psycopg2
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-# from ml2 import CrimePredictionModel
+from ml2 import CrimePredictionModel
 import os
 
 app = FastAPI()
@@ -14,12 +14,22 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 class Request(BaseModel):
     desc: list = []
     area: list = []
     number: str
     startDate: str
     endDate: str
+
+class PredictRequest(BaseModel):
+    desc: list = []
+    area: list = []
+    range: int
+
+class InputData(BaseModel):
+    data: list
+    cols: list
 
 def execute_query(query):
     conn = psycopg2.connect(
@@ -42,6 +52,8 @@ def query_request(request):
     area = request.area
     startDate = request.startDate
     endDate = request.endDate
+    print(startDate)
+    print(endDate)
     number = request.number
     area_conditions = []
     area_col = []
@@ -98,6 +110,53 @@ def query_request(request):
         """
     return query,cols
 
+def predict_query_request(request):
+    desc = request.desc   
+    area = request.area
+    number = request.range
+    area_conditions = []
+    area_col = []
+    for idx, item in enumerate(area):
+        value = item['value'][0]
+        condition = f"OR area_name = '{value}'"
+        if idx == 0:
+            condition = f"WHERE (area_name = '{value}'"
+        elif idx == len(area) - 1:
+            condition = f"{condition} OR area_name = '{value}')"
+        if len(area) == 1:
+            condition = f"WHERE area_name = '{value}'"
+        area_conditions.append(condition)
+        area_col.append(value)
+    # Creating variables for 'desc'
+    desc_conditions = []
+    desc_col = []
+    for idx, item in enumerate(desc):
+        value = item['value'][0]
+        condition = f"OR crm_cd_desc = '{value}'"
+        if idx == 0:
+            condition = f"AND (crm_cd_desc = '{value}'"
+        if idx == len(desc) - 1:
+            condition += ")"
+        if len(desc) == 1:
+            condition = f"AND crm_cd_desc = '{value}'"
+        desc_conditions.append(condition)
+        desc_col.append(value)
+
+    area_condition_str = ' '.join(area_conditions)
+    desc_condition_str = ' '.join(desc_conditions)
+    cols = 'crm_cd_desc, area_name, date_occ, location, lat, long, dr_no'
+    query = f"""
+        SELECT {cols}
+        FROM public.crime
+        JOIN public."crimeCode" ON public.crime.crm_cd = public."crimeCode".crm_cd
+        JOIN public.area ON public.crime.area = public.area.area
+        {area_condition_str}
+            {desc_condition_str}
+            AND date_occ >= current_date - interval '{number} days'
+        ORDER BY date_occ DESC;
+        """
+    return query,cols
+
 @app.get("/")
 def root():
     # Replace the placeholder query with your actual query
@@ -106,11 +165,6 @@ def root():
     query = "SELECT area_name FROM public.area"
     area = execute_query(query)
     return {"desc": desc, "area": area}
-
-@app.get("/wakeup")
-def root():
-    # Replace the placeholder query with your actual query
-    return 'ok'
 
 @app.post('/data')
 def plot_data(request: Request):
@@ -122,93 +176,17 @@ def plot_data(request: Request):
         print(e)
         raise HTTPException(status_code=500, detail=str(e))
     
-class InputData(BaseModel):
-    data: list
-    cols: list
-
-# @app.post('/predict')
-# def predict_data(data: InputData):
-#     try:
-#         cpm = CrimePredictionModel()
-#         query_req = cpm.runAll(data.data, data.cols, 1)
-#         return {"predict": query_req}
-#     except Exception as e:
-#         print(e)
-#         raise HTTPException(status_code=500, detail=str(e))
-
-
-
-# @app.post('/plot')
-# def plot_data(request: Request):
-#     try:
-#         query_req,cols = query_request(request)
-#         tot_crimes = execute_query(query_req)
-
-#         dates = list(set([entry[2].date().isoformat() for entry in tot_crimes]))  # Extract the date and convert to ISO format
-#         areas = list(set([entry[1] for entry in tot_crimes]))  # Assuming area_name is the second element (index 1)
-#         crimes = list(set([entry[0] for entry in tot_crimes]))  # Assuming area_name is the second element (index 1)
-#         # Count the occurrences of each area on each date
-#         counts = {date: {area: {crime: 0 for crime in crimes} for area in areas} for date in dates}
-
-#         for entry in tot_crimes:
-#             date = entry[2].date().isoformat()  # Extract the date and convert to ISO format
-#             area = entry[1]  # Assuming area_name is the second element (index 1)
-#             crime = entry[0]  # Assuming crime description is the first element (index 0)
-#             counts[date][area][crime] += 1
-#         # Prepare the stacked bar chart data
-
-#         dates = list(counts.keys())
-#         areas = list(counts[dates[0]].keys())
-#         crimes = list(counts[dates[0]][areas[0]].keys())
-#         dates.sort()
-#         # Create a dictionary to store the color mapping for each crime
-#         crime_colors = {crime: plt.cm.get_cmap('tab10')(idx) for idx, crime in enumerate(crimes)}
-
-
-#         num_bars = len(dates)
-#         bar_width = 0.35
-
-#         fig, ax = plt.subplots()
-#         fig.set_size_inches(10, 6)
-
-#         index = 0
-#         for i, date in enumerate(dates):
-#             crime_counts = [sum(counts[date][area][crime] for area in areas) for crime in crimes]
-#             bottom = [sum(crime_counts[:k]) for k in range(len(crime_counts))]
-#             for idx, val in enumerate(crime_counts):
-#                 ax.bar(
-#                     index, val, bar_width, bottom=bottom[idx],
-#                     label=crimes[idx], color=crime_colors[crimes[idx]]
-#                 )
-#             index += 1
-
-#         ax.set_xlabel('Date')
-#         ax.set_ylabel('Counts')
-#         ax.set_title(f'Combined Crime Counts in {areas} by Date')
-#         ax.set_xticks(range(num_bars))
-#         ax.set_xticklabels(dates)
-#         plt.xticks(rotation = 30) # Rotates X-Axis Ticks by 45-degrees
-#         handles, labels = ax.get_legend_handles_labels()
-#         unique_labels = list(set(labels))
-#         unique_handles = [handles[labels.index(label)] for label in unique_labels]
-#         box = ax.get_position()
-#         ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-#         # Put a legend to the right of the current axis
-#         ax.legend(unique_handles, unique_labels, loc='center left', bbox_to_anchor=(1, 0.5))
-
-#         plt.tight_layout()
-
-#         buffer = BytesIO()
-#         plt.savefig(buffer, format='png')
-#         plt.close(fig)
-#         buffer.seek(0)
-#         encoded_image = base64.b64encode(buffer.read()).decode()
-
-#         return {"crimes": tot_crimes, "plot": encoded_image, "cols": cols}
-#     except Exception as e:
-#         print(e)
-#         raise HTTPException(status_code=500, detail=str(e))
-
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.post('/predict2')
+def predict_data(request: PredictRequest):
+    try:
+        query_req,cols = predict_query_request(request)
+        cols_list = cols.split(', ')
+        tot_crimes = execute_query(query_req)
+        cpm = CrimePredictionModel()
+        print(cols)
+        print(tot_crimes)
+        query_req = cpm.runAll(tot_crimes, cols_list, 1)
+        return {"predict": query_req}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
--- ml2.py
+++ ml2.py
@@ -1,7 +1,11 @@
-import pandas as pd
 from config import engine
+import pandas as pd
 from tensorflow import keras
+from sklearn.model_selection import train_test_split
 from datetime import datetime, timedelta
+
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.svm import SVR
 
 class CrimePredictionModel:
     def __init__(self):
@@ -9,41 +13,121 @@ class CrimePredictionModel:
         self.history = None
         self.length = None
         self.totalLength = None
+        self.model2 = None
+
+    # def print_metrics(self, X_test, y_test, y_pred):
+    #     # Compute the confusion matrix
+    #     cm = confusion_matrix(y_test, y_pred)
+    #     print("Confusion Matrix:")
+    #     print(cm)
+
+    #     # Compute precision, recall, and F1 score
+    #     report = classification_report(y_test, y_pred)
+    #     print("Classification Report:")
+    #     print(report)
+
+    #     # Print test loss and accuracy
+    #     test_loss, test_accuracy = self.model.evaluate(X_test, y_test, verbose=1)
+    #     print("Test Loss:", test_loss)
+    #     print("Test Accuracy:", test_accuracy)
+
+    #     # Print validation accuracy
+    #     val_accuracy = self.history.history['val_accuracy']
+    #     print("Validation Accuracy:", val_accuracy)
+
+    # def parallel_coordinate_plot(self, X_test, y_pred, y_col_name):
+    #     df_pred = X_test.copy()
+    #     df_pred[y_col_name] = y_pred
+
+    #     fig = px.parallel_coordinates(df_pred, color=f'{y_col_name}')
+    #     fig.show()
 
     def process_data(self, query, cols):
-        df = pd.DataFrame(query, columns=cols)
-        df['crm_occ'] = 1
-        df['date_occ'] = pd.to_datetime(df['date_occ'])
-        self.length = df.shape[0]
-        df = df.drop(columns=["crm_cd_desc", "area_name", "location", "dr_no"])
-        coords = (df['lat'], df['long'])
-        df = df.drop(columns=["lat", "long"])
-        # Find the newest and oldest dates
-        newest_date = df['date_occ'].max()
-        oldest_date = df['date_occ'].min()
-        # Create a new DataFrame with minute-level granularity
-        minute_range = pd.date_range(start=oldest_date, end=newest_date, freq='min')
-        df_new = pd.DataFrame({'date_occ': minute_range})
-        # Merge the new DataFrame with the original DataFrame based on 'date_occ'
-        df_merged = pd.merge(df_new, df, on='date_occ', how='left')
-        # Fill missing values in 'crm_occ' with 0
-        df_merged['crm_occ'].fillna(0, inplace=True)
-        # Extract the year, month, day, hour, and minute into separate columns
-        df_merged['year'] = df_merged['date_occ'].dt.year
-        df_merged['month'] = df_merged['date_occ'].dt.month
-        df_merged['day'] = df_merged['date_occ'].dt.day
-        df_merged['hour'] = df_merged['date_occ'].dt.hour
-        df_merged['minute'] = df_merged['date_occ'].dt.minute
-        newest_date = df_merged['date_occ'].max()
-        oldest_date = df_merged['date_occ'].min()
-        df_merged = df_merged.drop('date_occ', axis=1)
-        self.totalLength = df_merged.shape[0]
-        # Split the data into features (X) and target variable (y)
-        X = df_merged.drop('crm_occ', axis=1)
-        y = df_merged['crm_occ']
-        return X, y
+        if not query: 
+            query = '''
+            SELECT date_occ
+            FROM public.crime
+            WHERE crm_cd IN (330, 410)
+            ORDER BY dr_no ASC
+            '''
+            df = pd.read_sql(query, engine)
+            # Close the engine connection if needed
+            engine.dispose()
+                    # Data processing
+            df['crm_occ'] = 1
+            df['date_occ'] = pd.to_datetime(df['date_occ'])
+            # Find the newest and oldest dates
+            newest_date = df['date_occ'].max()
+            oldest_date = df['date_occ'].min()
+            # Create a new DataFrame with minute-level granularity
+            minute_range = pd.date_range(start=oldest_date, end=newest_date, freq='min')
+            df_new = pd.DataFrame({'date_occ': minute_range})
+            # Merge the new DataFrame with the original DataFrame based on 'date_occ'
+            df_merged = pd.merge(df_new, df, on='date_occ', how='left')
+            # Fill missing values in 'crm_occ' with 0
+            df_merged['crm_occ'].fillna(0, inplace=True)
+            # Extract the year, month, day, hour, and minute into separate columns
+            df_merged['year'] = df_merged['date_occ'].dt.year
+            df_merged['month'] = df_merged['date_occ'].dt.month
+            df_merged['day'] = df_merged['date_occ'].dt.day
+            df_merged['hour'] = df_merged['date_occ'].dt.hour
+            df_merged['minute'] = df_merged['date_occ'].dt.minute
+            newest_date = df_merged['date_occ'].max()
+            oldest_date = df_merged['date_occ'].min()
+            df_merged = df_merged.drop('date_occ', axis=1)
 
-    def run_model(self, X_train, y_train):
+            # Split the data into features (X) and target variable (y)
+            X = df_merged.drop('crm_occ', axis=1)
+            y = df_merged['crm_occ']
+        else:
+            df = pd.DataFrame(query, columns=cols)
+            df['crm_occ'] = 1
+            df['date_occ'] = pd.to_datetime(df['date_occ'])
+            self.length = df.shape[0]
+            df = df.drop(columns=["crm_cd_desc", "area_name", "location", "dr_no"])
+            coords = (df['lat'], df['long'])
+            time_df = pd.DataFrame({'lat': coords[0], 'long': coords[1], 'date_occ': df['date_occ']})
+            time_df['year'] = time_df['date_occ'].dt.year
+            time_df['month'] = time_df['date_occ'].dt.month
+            time_df['day'] = time_df['date_occ'].dt.day
+            time_df['hour'] = time_df['date_occ'].dt.hour
+            time_df['minute'] = time_df['date_occ'].dt.minute
+            time_df = time_df.drop(columns=["date_occ"])
+            # Select the columns for the target variable (lat and long)
+            target_df = time_df[['lat', 'long']]
+            # Select the columns for the feature variables (year, month, day, hour, minute)
+            feature_df = time_df[['year', 'month', 'day', 'hour', 'minute']]
+            print(time_df)
+            df = df.drop(columns=["lat", "long"])
+            # Find the newest and oldest dates
+            newest_date = df['date_occ'].max()
+            oldest_date = df['date_occ'].min()
+            # Create a new DataFrame with minute-level granularity
+            minute_range = pd.date_range(start=oldest_date, end=newest_date, freq='min')
+            df_new = pd.DataFrame({'date_occ': minute_range})
+            # Merge the new DataFrame with the original DataFrame based on 'date_occ'
+            df_merged = pd.merge(df_new, df, on='date_occ', how='left')
+            # Fill missing values in 'crm_occ' with 0
+            df_merged['crm_occ'].fillna(0, inplace=True)
+            # Extract the year, month, day, hour, and minute into separate columns
+            df_merged['year'] = df_merged['date_occ'].dt.year
+            df_merged['month'] = df_merged['date_occ'].dt.month
+            df_merged['day'] = df_merged['date_occ'].dt.day
+            df_merged['hour'] = df_merged['date_occ'].dt.hour
+            df_merged['minute'] = df_merged['date_occ'].dt.minute
+            newest_date = df_merged['date_occ'].max()
+            oldest_date = df_merged['date_occ'].min()
+            df_merged = df_merged.drop('date_occ', axis=1)
+            self.totalLength = df_merged.shape[0]
+            # Split the data into features (X) and target variable (y)
+            X = df_merged.drop('crm_occ', axis=1)
+            y = df_merged['crm_occ']
+            return X, y, target_df, feature_df
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        return X_train, X_test, y_train, y_test
+
+    def run_model(self, X_train, y_train, target_df, feature_df):
         # Define the model architecture
         self.model = keras.Sequential([
             keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
@@ -55,7 +139,30 @@ class CrimePredictionModel:
         self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
         # Train the model
-        self.history = self.model.fit(X_train, y_train, epochs=10, batch_size=16, verbose=1, validation_split=0.2)
+        self.history = self.model.fit(X_train, y_train, epochs=2, batch_size=16, verbose=1, validation_split=0.2)
+
+        print(feature_df)
+        print(target_df)
+                # Define the model architecture
+        # self.model2 = keras.Sequential([
+        #     keras.layers.Dense(128, activation='relu', input_shape=(feature_df.shape[1],)),
+        #     keras.layers.Dense(64, activation='relu'),
+        #     keras.layers.Dense(32, activation='relu'),
+        #     keras.layers.Dense(2)  # Two output neurons for latitude and longitude, no activation function
+        # ])
+
+        # # Compile the model with mean squared error (MSE) as the loss function
+        # self.model2.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='mean_squared_error', metrics=['mean_squared_error'])
+        #         # Train the model
+        # self.history2 = self.model2.fit(feature_df, target_df, epochs=10, batch_size=16, verbose=1, validation_split=0.2)
+        print(feature_df)
+        print(target_df)
+
+        svr = SVR(kernel='poly')  # You can choose different kernels like 'linear', 'poly', or 'sigmoid'
+        self.model2 = MultiOutputRegressor(svr)
+        self.model2.fit(feature_df, target_df)
+
+
 
     def y_pred(self, X_test):
         # Evaluate the model on the test set
@@ -65,6 +172,18 @@ class CrimePredictionModel:
         value_at_percent = sorted_predictions[top_percent_index]
         y_pred = (y_pred_probs >= value_at_percent).astype(int)
         return y_pred
+    
+    def y_pred2(self, filtered_df):
+        occ_copy = filtered_df.copy()
+        occ_copy['year'] = occ_copy['date'].dt.year
+        occ_copy['month'] = occ_copy['date'].dt.month
+        occ_copy['day'] = occ_copy['date'].dt.day
+        occ_copy['hour'] = occ_copy['date'].dt.hour
+        occ_copy['minute'] = occ_copy['date'].dt.minute
+        occ_copy = occ_copy.drop(columns=['date'])
+        y_pred2_vals = self.model2.predict(occ_copy)
+        return y_pred2_vals
+        
 
     def localTest(self):
         X_train, X_test, y_train, y_test = self.process_data(None, None)
@@ -84,15 +203,21 @@ class CrimePredictionModel:
             'hour': date_range.hour,
             'minute': date_range.minute
         })
-        X_train, y_train = self.process_data(query, cols)
-        self.run_model(X_train, y_train)
+        X_train, y_train, target_df, feature_df = self.process_data(query, cols)
+        self.run_model(X_train, y_train, target_df, feature_df)
         df['crm_occ'] = self.y_pred(df)
         # Combine year, month, day, hour, and minute into a datetime column
         df['date'] = pd.to_datetime(df[['year', 'month', 'day', 'hour', 'minute']])
         # Round the datetime values to the nearest minute
         df['date'] = df['date'].dt.round('1min')
         filtered_df = df[df['crm_occ'] == 1][['date']]
-        return filtered_df['date'].astype(str).to_json(orient='values')
+        filtered_df.reset_index(drop=True, inplace=True)
+        y_pred_formatted = [[round(value, 6) for value in pair] for pair in self.y_pred2(filtered_df)]
+        latlong_preds = pd.DataFrame(y_pred_formatted, columns=['lat', 'long'])
+        combined_df = pd.concat([filtered_df, latlong_preds], axis=1)
+        combined_df['date'] = combined_df['date'].astype(str)
+        print(combined_df)
+        return combined_df.to_json(orient='values')
 
     
 # cpm = CrimePredictionModel()
